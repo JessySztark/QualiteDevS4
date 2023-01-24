2e5fc3fb642d332f6d417c339193fd82429fc371
Jessy Sztark <Jessy.Sztark@etu.univ-savoie.fr>
2023-01-24 15:00:35 +0100

    [1.0.1] Models files created

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,25 @@
+﻿namespace WSConvertisseur.Models {
+    public class Devise {
+		private int idDevise;
+		
+		private String? nomDevise;
+
+		private Double tauxDevise;
+        public Devise() {
+
+        }
+
+		public int IDDevise {
+			get { return idDevise; }
+			set { idDevise = value; }
+		}
+        public String? NomDevise {
+            get { return nomDevise; }
+            set { nomDevise = value; }
+        }
+        public Double TauxDevise {
+            get { return tauxDevise; }
+            set { tauxDevise = value; }
+        }
+    }
+}
